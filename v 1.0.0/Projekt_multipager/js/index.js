b7ad9a86840fa2a3f122d9b011c3dc151de957be
JimmyJ0ee <98901860+JimmyJ0ee@users.noreply.github.com>
2022-03-13 15:14:32 +0100

    Delete v 1.0.0 directory

--- v 1.0.0/Projekt_multipager/js/index.js
+++
@@ -1,8 +0,0 @@
-/**Funktion, um im gleichen Tab zu Profil zu navigieren */
-function weiterleiten(){
-    document.getElementById("index_info").innerHTML="Sie werden weitergeleitet... :)";
-    /**Verzögerung bei Seitenwechsel */
-    setTimeout(() => {
-        window.location.href = window.location.href.replace("index", "profile");
-    }, 500);
-}
