6cd6746189592ce951cd87ebc5fdc2df3bc3d307
chrixtixn <99743621+chrixtixn@users.noreply.github.com>
2022-03-14 22:41:22 +0100

    final_version_2

--- Projekt_multipager/js/welcome.js
+++ Projekt_multipager/js/welcome.js
@@ -2,7 +2,13 @@
 function weiterleiten(){
     window.localStorage.setItem("auth.user", null);
     document.getElementById("welcome_info").innerHTML="Diving...";
+    /**Sound, der gespielt wird */
+    var sound = new Audio("mp3/slow_trap.mp3");
+    sound.loop = true;
+    /*saudio.play();
+    window.open("C:/Users/Chris/Documents/Web_Eng_Project_WebFzion-main/Projekt_multipager/login.html");*/
     setTimeout(() => {
-        window.location.href = window.location.href.replace("welcome", "login");
+        sound.play();
+        window.open("C:/Users/Chris/Documents/Web_Eng_Project_WebFzion-main/Projekt_multipager/login.html");
     }, 500);
 }
