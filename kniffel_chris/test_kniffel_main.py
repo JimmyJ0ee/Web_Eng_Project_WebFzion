9f3fd3b89f6a79b885969a106a8b83e4a468cbc4
chrixtixn <99743621+chrixtixn@users.noreply.github.com>
2022-04-29 13:56:55 +0200

    update

--- kniffel_chris/test_kniffel_main.py
+++ kniffel_chris/test_kniffel_main.py
@@ -3,17 +3,16 @@ from unittest import TestCase
 from unittest.mock import patch
 import kniffel_main
 
-#class TestURLPrint(TestCase):
+#@patch("kniffel_main.input", create=True)
 
+#class test_main(TestCase):
     #fehler bei stdin: eof read line
-    #def test_main(self):
-    #    @mock.patch('table_handling.winner', return_value=1) #sagt def, dass func main 1 returnt
-    #    with patch('sys.stdout', new = StringIO()):
-    #        with patch('sys.stdin', new = StringIO('j\n0\n5')):
-    #            self.assertRaises(Exception, kniffel_main.main())
-    #@patch("kniffel_main.input", create=True)
+#    def test_main(self, mocked_input):
+#        mocked_input.side_effect = ['j']
+#        self.assertRaises(Exception, kniffel_main.main())
 
-    #fehler bei stdin: eof read line
-    #def test_main(self, mocked_input):
-    #    mocked_input.side_effect = ['j']
-    #    self.assertRaises(Exception, kniffel_main.main())
+
+
+        #"""with patch('sys.stdout', new = StringIO()):
+        #        with patch('sys.stdin', new = StringIO('j\n')):
+        #            self.assertRaises(Exception, kniffel_main.main())"""
